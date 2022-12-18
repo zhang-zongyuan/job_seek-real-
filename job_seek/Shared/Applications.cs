f830e2a3853cbcb39b5f27b8619b5479310accd0
zhang zong yuan <2100618d@student.tp.edu.sg>
2022-12-18 23:14:58 +0800

    Add project files.

---
+++ job_seek/Shared/Applications.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace job_seek.Client.Shared
+{
+    public class Applications
+    {
+        public int ApplicationId { get; set; }
+        public DateTime DateTime { get; set; }
+        public String Description { get; set; }
+    public Applications()
+    {
+
+    }
+
+    public Applications(int applicationId, DateTime dateTime, string description)
+    {
+            this.ApplicationId = applicationId;
+            this.DateTime = dateTime;
+            this.Description = description;
+    }
+    }
+}
+
